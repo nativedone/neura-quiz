ca8fb450b54b83059df6cea6fd217941d9770624
nativedone <adeildoamorim@gmail.com>
2022-04-12 09:36:59 -0300

    text new video

--- src/feature/home-page/background-video/background-video-mobile.js
+++ src/feature/home-page/background-video/background-video-mobile.js
@@ -3,7 +3,7 @@ import { styled } from "@theme";
 export function BackgroundVideoMobile({ start }) {
   return (
     <Video autoPlay muted loop playsInline className={start ? "zoom-out" : ""}>
-      <source src="/assets/video-2000k.mp4" type="video/mp4" />
+      <source src="/assets/neura-extended-video_1.mp4" type="video/mp4" />
     </Video>
   );
 }
@@ -11,10 +11,11 @@ export function BackgroundVideoMobile({ start }) {
 const Video = styled("video", {
   width: "100%",
   height: "100%",
-  transform: "scale(4) translateY(50%)",
-  transition: "all 700ms",
+  transform: "scale(1.8) translateY(35%)", 
+
+  transition: "all 900ms",
 
   "&.zoom-out": {
-    transform: "scale(0.5)  translateY(70%)",
+    transform: "scale(0.5) translateY(-1%)",
   },
 });
