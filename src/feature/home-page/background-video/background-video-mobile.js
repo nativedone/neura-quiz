26dc017772ca53765cabda08bf1fa68d4f57b0b9
nativedone <adeildoamorim@gmail.com>
2022-04-13 22:06:47 -0300

    update video

--- src/feature/home-page/background-video/background-video-mobile.js
+++ src/feature/home-page/background-video/background-video-mobile.js
@@ -1,6 +1,26 @@
+// import { useRef, useEffect } from 'react'
+
 import { styled } from "@theme";
 
 export function BackgroundVideoMobile({ hasStarted }) {
+  // const ref = useRef();
+
+  // useEffect(() => {
+  //   if(!ref || !ref.current) {
+  //     return
+  //   }
+
+  //   console.log("ref", ref)
+  //   console.log("ref.current", ref.current)
+
+  //   ref.current.playbackRate = 0.5;
+
+  //   // ref.current?.playbackRate = 0.5;
+  // })
+
+
+
+
   return (
     <Video autoPlay muted loop playsInline className={hasStarted ? "zoom-out" : ""}>
       <source src="/assets/neura-extended-video-480x1000.mp4" type="video/mp4" />
