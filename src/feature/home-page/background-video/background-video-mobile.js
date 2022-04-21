8534168ad90c1acc47081fff26e3e6cef8475aca
nativedone <adeildoamorim@gmail.com>
2022-04-21 20:17:31 -0300

    update video

--- src/feature/home-page/background-video/background-video-mobile.js
+++ src/feature/home-page/background-video/background-video-mobile.js
@@ -45,14 +45,13 @@ export function BackgroundVideoMobile() {
 }
 
 const Video = styled("video", {
-  width: "200vw",
-  height: "200vh", // set the expected aspect ratio based on viewport width
-  // height: "auto",
-  // transform: "scale(1.8) translateY(20%)", 
-
-  //   transition: "transform 1200ms", // 1.2 sec
+  width: "100%",
+  height: "100%",
+  transform: "scale(1.8) translateY(20%)", 
   
-  // "&.zoom-out": {
-  //   transform: "scale(0.5) translateY(-27%)",
-  // },
+  transition: "transform 1200ms", // 1.2 sec
+  
+  "&.zoom-out": {
+    transform: "scale(0.5) translateY(-27%)",
+  },
 });
